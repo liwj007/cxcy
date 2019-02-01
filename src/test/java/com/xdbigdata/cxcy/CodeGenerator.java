f3a50f26b8781580661c54d5cc6c13b0e1838324
liwenjun <liwj007@163.com>
2019-02-01 13:09:15 +0800

    init

---
+++ src/test/java/com/xdbigdata/cxcy/CodeGenerator.java
@@ -0,0 +1,51 @@
+package com.xdbigdata.cxcy;
+
+import org.junit.Test;
+
+import com.xdbigdata.framework.mybatis.utils.MybatisGeneratorUtils;
+import com.xdbigdata.framework.service.utils.ServiceGeneratorUtils;
+
+/**
+ * Project code genertor
+ * 
+ * @author lshaci
+ * @since 0.0.3
+ */
+public class CodeGenerator {
+	/**
+	 * the project domain package
+	 */
+	private String domainPackage = "com.xdbigdata.cxcy.model.domain";
+	
+	/**
+	 * the project service package
+	 */
+	private String servicePackage = "com.xdbigdata.cxcy.service";
+	
+	/**
+	 * the project mapper package
+	 */
+	private String mapperPackage = "com.xdbigdata.cxcy.mapper";
+
+	/**
+	 * Generator domain, mapper interface, mapper xml file with framework mybatis generator util 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void generatorWithFrameworkMybatis() throws Exception {
+		MybatisGeneratorUtils.generator(CodeGenerator.class);
+	}
+	
+	/**
+	 * Generator domain, mapper interface, mapper xml file, service interface, service implement <br>
+	 * with framework service generator util 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void generatorWithFrameworkService() throws Exception {
+		ServiceGeneratorUtils.generator(CodeGenerator.class, servicePackage, domainPackage, mapperPackage);
+	}
+	
+}
